fa69ca1f049120e4e8eb54647613280f7876126a
melanga <dissanayakedmmb@gmail.com>
2020-10-15 21:17:09 +0530

    Solved a problem of black images background also turning black while converting

--- new.py
+++ new.py
@@ -1,15 +1,30 @@
-from PIL import Image
 from os import listdir
+from PIL import Image
 
-image_list = listdir("images/")
-print(image_list)
-# Image.open("images/" + image_list[1]).rotate(90).show()
-for one_image in image_list[1:]:
-    infile = "images/" + one_image
-    outfile = one_image
-    if infile != outfile:
-        try:
-            with Image.open(infile) as im:
-                im.rotate(90).save(outfile)
-        except OSError:
-            print("cannot convert", infile)
+
+def convertBlackImages(name):
+	im = Image.open("images/" + name)
+	out = im.rotate(270).resize((128,128)).convert("RGBA")
+	background = Image.new('RGBA', out.size, (255,255,255))
+	final = Image.alpha_composite(background, out).convert("RGB")
+	final.save("c_images/" + name + ".jpg", "JPEG")
+
+
+def convertWhiteImages(name):
+	im = Image.open("images/" + name)
+	final = im.rotate(270).resize((128,128)).convert("RGB")
+	final.save("c_images/" + name + ".jpg", "JPEG")
+
+
+def main():
+	img_list = listdir("images/")
+	for img_name in img_list:
+		if "ic" in img_name:
+			if "black" in img_name:
+				convertBlackImages(img_name)
+			else:
+				convertWhiteImages(img_name)
+
+
+if __name__ == '__main__':
+    main()

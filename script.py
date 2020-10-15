22bcc95e8437e55e6221f578f68ade18f054ac75
melanga <dissanayakedmmb@gmail.com>
2020-10-16 04:11:35 +0530

    Added PEP8 guidelines.

--- script.py
+++ script.py
@@ -3,38 +3,38 @@ from os import listdir
 from PIL import Image
 
 
-def convertBlackImages(name):
+def convert_black_images(name):
 	im = Image.open("images/" + name)
-	out = im.rotate(270).resize((128,128)).convert("RGBA")
-	background = Image.new('RGBA', out.size, (255,255,255))
+	out = im.rotate(270).resize((128, 128)).convert("RGBA")
+	background = Image.new('RGBA', out.size, (255, 255, 255))
 	final = Image.alpha_composite(background, out).convert("RGB")
 	final.save("c_images/" + name + ".jpg", "JPEG")
 
 
-def convertWhiteImages(name):
+def convert_white_images(name):
 	im = Image.open("images/" + name)
-	final = im.rotate(270).resize((128,128)).convert("RGB")
+	final = im.rotate(270).resize((128, 128)).convert("RGB")
 	final.save("c_images/" + name + ".jpg", "JPEG")
 
 
-def processImages():
+def process_images():
 	img_list = listdir("images/")
 	for img_name in img_list:
 		if "ic" in img_name:
 			if "black" in img_name:
-				convertBlackImages(img_name)
+				convert_black_images(img_name)
 			else:
-				convertWhiteImages(img_name)
+				convert_white_images(img_name)
 
 
 def main():
 	path = "/home/melanga/Desktop/week1/resizeandconvert/c_images"
 	try: 
 		os.mkdir(path)
-		processImages()
+		process_images()
 	except OSError as error:
-		processImages()
+		process_images()
 
 
 if __name__ == '__main__':
-    main()
+	main()
